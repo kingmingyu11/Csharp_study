2285f4ef17b52b39c19be10f303896da215cd955
kingmingyu11 <137870189+kingmingyu11@users.noreply.github.com>
2023-06-28 16:26:48 +0900

    Update forfor.cs

--- Day002/forfor.cs
+++ Day002/forfor.cs
@@ -1,1 +1,31 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forfor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            for(int i = 0; i <25; i++)
+            {
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+            cnt = 0;
+            for (int i =0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt); 
+        }
+    }
+}
